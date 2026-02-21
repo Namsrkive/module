2572f3c8c3c6c25baf4293ac9cca7499a1876d60
Riya Malik <riyamalik0508@gmail.com>
2026-02-21 19:09:15 +0530

    frontend

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Proctored Placement Readiness Portal</h1>
+      <p>Frontend is working 🚀</p>
+    </div>
+  );
+}
+
+export default App;
